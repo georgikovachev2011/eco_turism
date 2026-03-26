a710d0ff851f3a2a134d66b97842302223175238
AG6555 <alexgueroguiev@gmail.com>
2026-03-26 22:33:11 +0200

    deleted_signup

--- Backend/django/project/sign_up/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Sign_up(models.Model):
-    username = models.CharField(max_length=100)
-    email = models.EmailField()
-    password = models.CharField(max_length=16)
-
- 
--- Backend/django/project/sign_up/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-
-class SignUpSerializer(serializers.Serializer):
-    username = serializers.CharField(max_length=100)
-    email = serializers.EmailField()
-    password_1 = serializers.CharField()
-    password_2 = serializers.CharField()
