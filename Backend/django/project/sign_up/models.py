cca5ac81b75977edd548d14805e6d3d21305c497
Gunter-hub <bojidarang7@gmail.com>
2026-03-26 22:26:22 +0200

    Trying_to_troubleshoot_sign_up_again

--- Backend/django/project/sign_up/models.py
+++ Backend/django/project/sign_up/models.py
@@ -3,6 +3,6 @@ from django.db import models
 class Sign_up(models.Model):
     username = models.CharField(max_length=100)
     email = models.EmailField()
-    password = models.CharField()
+    password = models.CharField(max_length=16)
 
  
--- Backend/django/project/sign_up/views.py
+++ Backend/django/project/sign_up/views.py
@@ -1,23 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from sign_up.models import Signup
+from sign_up.models import Sign_up
 from rest_framework.decorators import api_view
-from .serializers import Sign_upSerializer
+from .serializers import SignUpSerializer
 from drf_yasg.utils import swagger_auto_schema
 
 @swagger_auto_schema(
         method="post",
-        request_body=Sign_upSerializer
+        request_body=SignUpSerializer
 )
 
 @api_view(["POST"])
-def sign_up(request):
-    serializer = Sign_upSerializer(data=request.data)
+def signup(request):
+    serializer = SignUpSerializer(data=request.data)
     serializer.is_valid()
-    sign_up = Signup(name = serializer.data['name'],
-                      username = serializer.data['email'],
-                      email = serializer.data['phone_num'],
-                      password = serializer.data['date_start'], )
+    sign_up = Sign_up(name = serializer.data['name'],
+                      username = serializer.data['username'],
+                      email = serializer.data['email'],
+                      password = serializer.data['password'], )
     
     sign_up.save()
     return HttpResponse()
