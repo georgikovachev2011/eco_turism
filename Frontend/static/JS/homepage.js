e2119c9c1aa2137fef8b06d866c76d0dcdfceebc
georgikovachev2011 <georgi.kovachev2011@gmail.com>
2026-03-26 22:54:12 +0200

    stop it pls

--- Frontend/static/JS/homepage.js
+++ Frontend/static/JS/homepage.js
@@ -1,15 +1,17 @@
-const burger = document.querySelector(".burger");
-const mobileMenu = document.querySelector(".mobile-menu");
-const closeMenu = document.querySelector(".close-menu");
+document.addEventListener("DOMContentLoaded", () => {
+    const burger = document.querySelector(".burger");
+    const mobileMenu = document.querySelector(".mobile-menu");
+    const closeMenu = document.querySelector(".close-menu");
 
-if (burger && mobileMenu) {
-    burger.addEventListener("click", () => {
-        mobileMenu.classList.add("active");
-    });
-}
+    if (burger && mobileMenu) {
+        burger.addEventListener("click", () => {
+            mobileMenu.classList.add("active");
+        });
+    }
 
-if (closeMenu && mobileMenu) {
-    closeMenu.addEventListener("click", () => {
-        mobileMenu.classList.remove("active");
-    });
-}
+    if (closeMenu && mobileMenu) {
+        closeMenu.addEventListener("click", () => {
+            mobileMenu.classList.remove("active");
+        });
+    }
+});
