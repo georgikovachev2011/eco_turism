ef712c6a5930222e6f43a583b1595d2c620b1b42
georgikovachev2011 <georgi.kovachev2011@gmail.com>
2026-03-26 22:16:22 +0200

    homepage is nearly ready

--- Frontend/static/JS/homepage.js
+++ Frontend/static/JS/homepage.js
@@ -1,6 +1,15 @@
 const burger = document.querySelector(".burger");
-const navLinks = document.querySelector(".nav-links");
+const mobileMenu = document.querySelector(".mobile-menu");
+const closeMenu = document.querySelector(".close-menu");
 
-burger.addEventListener("click", () => {
-    navLinks.classList.toggle("active");
-});
+if (burger && mobileMenu) {
+    burger.addEventListener("click", () => {
+        mobileMenu.classList.add("active");
+    });
+}
+
+if (closeMenu && mobileMenu) {
+    closeMenu.addEventListener("click", () => {
+        mobileMenu.classList.remove("active");
+    });
+}
