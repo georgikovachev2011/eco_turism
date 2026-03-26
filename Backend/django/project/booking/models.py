96a6eb5bbb107e2948e4d0277c4fd3c133d000e8
AG6555 <alexgueroguiev@gmail.com>
2026-03-26 15:32:13 +0200

    resolved_merge_conflict

--- Backend/django/project/booking/models.py
+++ Backend/django/project/booking/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Booking(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone_num = models.CharField()
+    date_start = models.DateField()
+    date_end = models.DateField()
+    hotel_num = models.IntegerField()
+    id = models.AutoField(primary_key=True)
+
--- Backend/django/project/booking/views.py
+++ Backend/django/project/booking/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from models import Booking
 
-# Create your views here.
+def book(request):
+    #add validations
+    booking = Booking(name = request.name,
+                      email = request.email,
+                      phone_num = request.phone_num,
+                      date_start = request.date_start,
+                      date_end = request.date_end,
+                      hotel_id = request.hotel_id )
+    booking.save()
+    return HttpResponse()
+
