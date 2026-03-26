2ac6257fdcf24395d3438229594cc9ec7766a70b
Gunter-hub <bojidarang7@gmail.com>
2026-03-26 14:01:35 +0200

    Connecting_be_fe

--- Backend/django/project/main/settings.py
+++ Backend/django/project/main/settings.py
@@ -54,7 +54,9 @@ ROOT_URLCONF = 'main.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR.parent.parent.parent / "Frontend" / "HTML"
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -114,4 +116,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR.parent.parent.parent / "Frontend" / "static"
+]
