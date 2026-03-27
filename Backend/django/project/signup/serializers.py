a7a41fdefb1cdc69158522f639b6aafdfeacf92e
AG6555 <alexgueroguiev@gmail.com>
2026-03-27 09:48:11 +0200

    Signup_400

--- Backend/django/project/signup/serializers.py
+++ Backend/django/project/signup/serializers.py
@@ -1,9 +1,7 @@
 from rest_framework import serializers
 
 class SignupSerializer(serializers.Serializer):
-    name = serializers.CharField(max_length=100)
+    username = serializers.CharField(max_length=100)
     email = serializers.EmailField()
-    phone_num = serializers.CharField()
-    date_start = serializers.DateField()
-    date_end = serializers.DateField()
-    hotel_num = serializers.IntegerField()
+    password1 = serializers.CharField()
+    password2 = serializers.CharField()
