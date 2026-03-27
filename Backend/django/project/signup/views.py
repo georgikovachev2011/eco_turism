f80a6349149e3b64c93f19d97b7f40e9db45d338
AG6555 <alexgueroguiev@gmail.com>
2026-03-27 09:31:59 +0200

    signup_1/2done

--- Backend/django/project/signup/views.py
+++ Backend/django/project/signup/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-#from booking.models import Booking
+from signup.models import Signup
 from rest_framework.decorators import api_view
-#from .serializers import BookingSerializer
+from . serializers import SignupSerializer
 from drf_yasg.utils import swagger_auto_schema
+
+@swagger_auto_schema(
+        method="post",
+        request_body=SignupSerializer
+)
+@api_view(["POST"])
+def book(request):
+    serializer = SignupSerializer(data=request.data)
+    serializer.is_valid()
+    #add validations
+    signup = Signup(name = serializer.data['name'],
+                      email = serializer.data['email'],
+                      phone_num = serializer.data['phone_num'],
+                      date_start = serializer.data['date_start'],
+                      date_end = serializer.data['date_start'],
+                      hotel_num = serializer.data['hotel_num'] )
+    signup.save()
+    return HttpResponse()
