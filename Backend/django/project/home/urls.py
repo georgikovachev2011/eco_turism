bc18abb83f572ea48cd9720938483a66749d7876
AG6555 <alexgueroguiev@gmail.com>
2026-03-27 16:45:36 +0200

    Path_and_resetpassword

--- Backend/django/project/home/urls.py
+++ Backend/django/project/home/urls.py
@@ -2,5 +2,15 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('', views.home, name="homepage")
+    path('home/', views.home, name="homepage"),
+    path('about/', views.about, name="about"),
+    path('contaxts/', views.contaxts, name="contaxts"),
+    path('email_sent/', views.email_sent, name="email_sent"),
+    path('favorites/', views.favorites, name="favorites"),
+    path('forgot_password/', views.forgot_password, name="forgot_password"),
+    path('pay/', views.pay, name="pay"),
+    path('reset_password/', views.reset_password, name="reset_password"),
+    path('destination/', views.destination, name="destination"),
+    path('sign_up/', views.sign_up, name="sign_up"),
+    path('sign_in/', views.sign_in, name="sign_in"),
 ]
--- Backend/django/project/main/urls.py
+++ Backend/django/project/main/urls.py
@@ -34,7 +34,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', include('home.urls')),
+    path('', include('home.urls')),
     path('booking/', include('booking.urls')),
     path('service/', include('service.urls')),
     path('users/',include('users.urls')),
